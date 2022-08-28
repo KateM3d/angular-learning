c60743394b3f952785eee5463203a4a6a0d234ef
KateM3d <ekaterina8medvedeva@gmail.com>
2022-08-28 11:59:44 -0400

    add renderer and add hostlistener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [
-    { type: 'server', name: 'test server', content: 'just a test' },
-  ];
-
-  onServerAdded(serverData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent,
-    });
-  }
-
-  onBlueprintAdded(blueprintData: {
-    serverName: string;
-    serverContent: string;
-  }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent,
-    });
-  }
+  // serverElements = [
+  //   { type: 'server', name: 'test server', content: 'just a test' },
+  // ];
+  //   onServerAdded(serverData: { serverName: string; serverContent: string }) {
+  //     this.serverElements.push({
+  //       type: 'server',
+  //       name: serverData.serverName,
+  //       content: serverData.serverContent,
+  //     });
+  //   }
+  //
+  //   onBlueprintAdded(blueprintData: {
+  //     serverName: string;
+  //     serverContent: string;
+  //   }) {
+  //     this.serverElements.push({
+  //       type: 'blueprint',
+  //       name: blueprintData.serverName,
+  //       content: blueprintData.serverContent,
+  //     });
+  //   }
+  numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { ComponentOneComponent } from './component-one/component-one.component';
 import { ComponentTwoComponent } from './component-two/component-two.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-directive/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentOneComponent,
-    ComponentTwoComponent
+    ComponentTwoComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
